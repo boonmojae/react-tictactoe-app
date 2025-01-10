e45c0f2586c71991536478b7d5d6e0db696e6502
boonmojae <pminny99@naver.com>
2025-01-11 03:08:26 +0900

    ch04 start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import './App.css';//강의는 이걸 추가안해도 css적용되는데 나는 이게 있어야 적용된다.
+import { useState } from 'react';
+import './App.css';
 import Board from './components/Board';
 function App() {
+
+  const [history, setHistory] = useState([ {squares: Array(9).fill(null )}]);
+  const [xIsNext , setXIsNext] = useState(true);
+  
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,4],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let index = 0; index < lines.length; index++) {
+      const [a,b,c] = lines[index];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if(winner) {
+    status = 'Winner: ' + winner;
+  }else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();//.squares를 하면 현재 squares값을 가질 수 있고 
+    // slice는 원본을 바로 직접 수정하는게 아닌 복사본을 만들어서 수정한 다음 넣어주려고 하는것이고 state의 불변성을 지켜줌
+    if(calculateWinner(newSquares) || newSquares[i]){//newSquares가 있고 아니면 newSquares가 클릭되어있는곳을 클릭하면
+      return;
+    }
+
+    //만약 그렇지 않다면
+    //아직 승부가 나지 않고 클릭이 안된부분을 클릭했다면
+    newSquares[i] = xIsNext? 'X' : 'O';
+    setHistory([...history, {squares: newSquares}])//history도 원본을 넣어주는게 아닌 복사해서 하나씩 넣어준다(전개연산자를 사용해서)
+    setXIsNext(prev => !prev);//true->false, false->true
+  }
+
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)}/>
       </div>
       <div className="game-info">
-        game-info
+        <div className="status">{status}</div>
       </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,55 +1,16 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-const Board = () => {
-
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext , setXIsNext] = useState(true)
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,4],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    for (let index = 0; index < lines.length; index++) {
-      const [a,b,c] = lines[index];
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if(winner) {
-    status = 'Winner: ' + winner;
-  }else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXIsNext(prev => !prev)
-  }
-
+const Board = ({squares, onClick}) => {//(squares, onClick)로 되어있으면 매개변수 전달로 props를 디스트럭처링 하는게 아니다
+  
   const renderSquare = (i) => {
       return <Square value={squares[i]}
-        onClick={() => handleClick(i)} />
+        onClick={() => onClick(i)} />
   }
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className='board-row'>
             {renderSquare(0)}
             {renderSquare(1)}
