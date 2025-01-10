41eda26d0e6d51f3986f23ed728dcf991d314728
boonmojae <pminny99@naver.com>
2025-01-10 16:12:01 +0900

    함수형 컴포넌트로 변경경

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import Square from './Square'
+import "./Board.css";
 
 export default class Board extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {//i로 인덱스 값을 받아옴
+    const squares = this.state.squares.slice();//하나를 새로 복사해줄 때 사용하는게 slice메서드_메소드에 아무것도 안넣으면 모든 square의 배열을 새롭게 복사
+    squares[i] = 'X';
+    this.setState({squares: squares})//state을 변경할땐 항상 setState를 사용
+  }
+
   renderSquare(i) {
-      return <Square />
+      return <Square value={this.state.squares[i]}//square 자식 컴포넌트에 props로 내려준것
+        onClick={() => this.handleClick(i)} />
   }
 
   render() {
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Square.css";
+
 
 export default class Square extends React.Component {
+
     render() {
         return (
-            <button className="square">
-                Square
+            <button className="square" onClick={() => { this.props.onClick() }}>
+                {this.props.value}
             </button>
         )
     }
